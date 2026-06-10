b4ea752a7e9a4d0ddf2d876e17ff27686abd77d3
BISAYOT <bisayotduligas@gmail.com>
2026-06-10 12:50:01 +0800

    duplicate

--- backend/backend/check_schema.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'public/index.php';
-$db = \Config\Database::connect();
-echo "Activity Design:\n";
-print_r($db->getFieldNames('activity_design'));
-echo "\nAccomplishment Report:\n";
-print_r($db->getFieldNames('accomplishment_report'));
